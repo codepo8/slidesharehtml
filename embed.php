ad5c17793eeca6b3bbe2b5d67ebadb0806ccd3ba
Christian Heilmann <chris.heilmann@gmail.com>
2010-11-11 23:37:06 +0000

    intro page added

---
+++ embed.php
@@ -0,0 +1,85 @@
+<?php
+/*
+  Slideshare HTML Version
+  Uses YQL for scraping and conversion
+  Homepage: http://github.com/codepo8/slidesharehtml
+  Copyright (c) 2010 Christian Heilmann
+  Code licensed under the BSD License:
+  http://wait-till-i.com/license.txt
+*/
+if(!ob_start("ob_gzhandler")) ob_start();
+error_reporting(0);
+$url = $_GET['url'];
+$url = preg_replace('/.net\//','.net/mobile/',$url);
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL, $url); 
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Linux; U; Android 2.1-update1; de-de; HTC Desire 1.19.161.5 Build/ERE27) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$images = curl_exec($ch); 
+curl_close($ch);
+preg_match_all('/"baseSlideUrl":"([^"]+)"/',$images,$slides);
+preg_match_all('/"totalSlides":(\d+),/',$images,$numbers);
+$num = $numbers[1][0];
+$slides = $slides[1][0];
+$yay = ($num && $slides);
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  
+  <title>HTML Slideshare embed</title>
+  <link rel="stylesheet" href="slidesharehtml.css" type="text/css">
+</head>
+<body>
+<?php if($yay){ ?>
+<div id="slideshare"><form>
+  <img id="slide" src="<?php echo $slides;?>-slide-1.jpg" alt="">
+  <div>
+    <span id="back">
+      <input type="button" id="first" value="first" disabled="disabled">
+      <input type="button" id="prev" value="previous" disabled="disabled">
+    </span>
+    <span id="count">1/<?php echo $num;?></span>
+    <span id="fwd">
+      <input type="button" value="next" id="next">
+      <input type="button" value="last" id="last">
+    </span>
+  </div>
+</form></div>
+<script src="http://yui.yahooapis.com/combo?3.2.0pr1/build/yui/yui-min.js"></script>
+<script>
+YUI().use('node', function(Y) {
+  var current = 1,
+      all = <?php echo $num;?>,
+      img = Y.one('#slide'),
+      url = img.get('src').replace(/\d\.jpg/,'');
+  Y.one('#slideshare').delegate('click',function(event){
+    var id = this.get('id');
+    switch(id){
+      case 'next':current++;break;
+      case 'prev':current--;break;
+      case 'last':current = all;break;
+      case 'first':current = 1;break;
+    }
+    Y.one('#count').set('innerHTML',current+'/'+all);
+    if(current === all){
+      Y.one('#last').set('disabled','disabled');
+      Y.one('#next').set('disabled','disabled');
+    } else {
+      Y.one('#last').removeAttribute('disabled');
+      Y.one('#next').removeAttribute('disabled');
+    }
+    if(current === 1){
+      Y.one('#first').set('disabled','disabled');
+      Y.one('#prev').set('disabled','disabled');
+    } else {
+      Y.one('#first').removeAttribute('disabled');
+      Y.one('#prev').removeAttribute('disabled');
+    }
+    img.set('src',url + current + '.jpg');
+  },'input');
+});
+</script>
+<?php }?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,85 +1,66 @@
 <?php
-/*
-  Slideshare HTML Version
-  Uses YQL for scraping and conversion
-  Homepage: http://github.com/codepo8/slidesharehtml
-  Copyright (c) 2010 Christian Heilmann
-  Code licensed under the BSD License:
-  http://wait-till-i.com/license.txt
-*/
-if(!ob_start("ob_gzhandler")) ob_start();
-error_reporting(0);
-$url = $_GET['url'];
-$url = preg_replace('/.net\//','.net/mobile/',$url);
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, $url); 
-curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Linux; U; Android 2.1-update1; de-de; HTC Desire 1.19.161.5 Build/ERE27) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-$images = curl_exec($ch); 
-curl_close($ch);
-preg_match_all('/"baseSlideUrl":"([^"]+)"/',$images,$slides);
-preg_match_all('/"totalSlides":(\d+),/',$images,$numbers);
-$num = $numbers[1][0];
-$slides = $slides[1][0];
-$yay = ($num && $slides);
+  if(isset($_GET['url'])){
+    if(!preg_match('/^http:\/\/www.slideshare.net/',$_GET['url'])){
+    $url = 'error';
+    } else {
+      $url = $_GET['url'];
+    }
+  } 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  
-  <title>HTML Slideshare embed</title>
-  <link rel="stylesheet" href="slidesharehtml.css" type="text/css">
+  <title>Embedding Slideshare presentations as HTML/JavaScript</title>
+  <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/combo?2.8.0/build/reset-fonts-grids/reset-fonts-grids.css&2.8.0/build/base/base-min.css">
+  <style type="text/css">
+  iframe{border:none;width:500px;height:300px;}
+  textarea{padding:10px;width:700px;border:none;
+  -webkit-box-shadow:0px 0px 20px rgba(33,123,232,.8);
+  -moz-box-shadow:0px 0px 20px rgba(33,123,232,.8)
+  }
+
+  html,body{background:#9cf;color:#000}
+  #doc{background:#fff;border:1em solid #fff;
+  -moz-box-shadow:2px 2px 10px rgba(0,0,0,.8);
+  -webkit-box-shadow:2px 2px 10px rgba(0,0,0,.8)
+  }
+  #ft{font-size:80%;margin-top:3em;text-align:right;}
+  h1,h2{font-family:"arial rounded mt bold",arial,sans-serif}
+  h1{color:#369;font-size:200%}
+  h2{color:#333;margin:2em 0;}
+  .error{color:#c00;margin:2em 0;font-weight:bold;}
+  h2 span{color:#fff;background:#393;-moz-border-radius:20px;padding:10px 15px;}
+  a{color:#369;}  </style>
 </head>
-<body>
-<?php if($yay){ ?>
-<div id="slideshare"><form>
-  <img id="slide" src="<?php echo $slides;?>-slide-1.jpg" alt="">
-  <div>
-    <span id="back">
-      <input type="button" id="first" value="first" disabled="disabled">
-      <input type="button" id="prev" value="previous" disabled="disabled">
-    </span>
-    <span id="count">1/<?php echo $num;?></span>
-    <span id="fwd">
-      <input type="button" value="next" id="next">
-      <input type="button" value="last" id="last">
-    </span>
+<body class="yui-skin-sam">
+<div id="doc" class="yui-t7">
+
+  <div id="hd" role="banner">
+    <h1>Creating a Slideshare HTML/JS embed</h1>
   </div>
-</form></div>
-<script src="http://yui.yahooapis.com/combo?3.2.0pr1/build/yui/yui-min.js"></script>
-<script>
-YUI().use('node', function(Y) {
-  var current = 1,
-      all = <?php echo $num;?>,
-      img = Y.one('#slide'),
-      url = img.get('src').replace(/\d\.jpg/,'');
-  Y.one('#slideshare').delegate('click',function(event){
-    var id = this.get('id');
-    switch(id){
-      case 'next':current++;break;
-      case 'prev':current--;break;
-      case 'last':current = all;break;
-      case 'first':current = 1;break;
-    }
-    Y.one('#count').set('innerHTML',current+'/'+all);
-    if(current === all){
-      Y.one('#last').set('disabled','disabled');
-      Y.one('#next').set('disabled','disabled');
-    } else {
-      Y.one('#last').removeAttribute('disabled');
-      Y.one('#next').removeAttribute('disabled');
-    }
-    if(current === 1){
-      Y.one('#first').set('disabled','disabled');
-      Y.one('#prev').set('disabled','disabled');
-    } else {
-      Y.one('#first').removeAttribute('disabled');
-      Y.one('#prev').removeAttribute('disabled');
-    }
-    img.set('src',url + current + '.jpg');
-  },'input');
-});
-</script>
+  <div id="bd" role="main">
+    <p>Slideshare is a great system to host presentations for distribution. The thing about it that keeps some folk from using it is that it uses Flash. Fear not, for here you can create a plain HTML/JavaScript/CSS embed version of Slideshare presentations. Simply follow the steps below:</p>
+    <h2><span>1</span> Give us the URL of the Slideshare presentation</h2>
+    <form action="index.php">
+      <div><label for="url">The Slideshare URL:</label><input type="url" name="url" id="url"><input type="hidden" name="generate" value="yes"><input type="submit" value="give me the embed!"></div>
+    </form>
+<?php if(isset($_GET['generate']) && $url!='error'){?>
+<h2><span>2</span> Does the preview look right?</h2>
+<iframe src="embed.php?url=<?php echo $url;?>"></iframe>
+<h2><span>3</span> Your embed for copy+paste:</h2>
+<form><textarea>
+&lt;iframe src="http://icant.co.uk/slidesharehtml/embed.php?url=<?php echo $url;?>"&gt;&lt;/iframe&gt;
+</textarea></form>
+<?php } if($url=='error') {?>
+  <p class="error">This is not a valid Slideshare url it seems :(</p>
 <?php }?>
+
+  </div>
+  <div id="ft" role="contentinfo">
+    <p>Written by <a href="http://wait-till-i.com">Chris Heilmann</a></p>
+  </div>
+</div>
 </body>
 </html>
+
