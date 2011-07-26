83b114e7d2227e90d6dbb49845c81e0c065dea09
vicchi <gary@vicchi.org>
2011-07-26 15:39:09 +0200

    Add configurable settings for self hosted domain name and path.

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+/** Start of configurable settings */
+
+/**
+ * If you're self-hosting slidesharehtml ...
+ * 1) Modify HOSTED_DOMAIN to the FQDN of your server root
+ * 2) Modify HOSTED_PATH to the relative path where you're hosting the slidesharehtml files
+*/
+
+define('HOSTED_DOMAIN', 'http://icant.co.uk');
+
+define('HOSTED_PATH', 'slidesharehtml');
+
+/**
+ * If you're not self-hosting slidesharehtml then move on, there's nothing to see, this should
+ * work "out of the box"
+ */
+
+define('SLIDESHAREHTML_URL', HOSTED_DOMAIN . DIRECTORY_SEPARATOR . HOSTED_PATH);
+
+?>
